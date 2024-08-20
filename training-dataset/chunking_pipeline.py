5c482a0da195e2bf08d59dba1c28b21c826589fe
TMihajlov96 <teodoramihajlov@gmail.com>
2024-08-20 17:21:19 +0200

    Add parallel request to chunking.

--- training-dataset/chunking_pipeline.py
+++ training-dataset/chunking_pipeline.py
@@ -1,17 +1,32 @@
 import os
+import sys
 from typing import Dict, List
 import numpy as np
-import numpy as np
 import openai
-import random
 import tiktoken
+import json
 
 from sklearn.metrics.pairwise import cosine_similarity
 from pprint import pprint
 from statistics import median
-from langchain_community.embeddings import OpenAIEmbeddings
+from pathlib import Path
+sys.path.append('..')
+from api_request_parallel_processor import run_api_request_processor
 
-oaiembeds = OpenAIEmbeddings(model='text-embedding-3-small')
+def save_jobs(sentences: List[Dict[str, str]], filename: Path, model: str = "text-embedding-3-small") -> None:    
+
+    jobs = [
+                {
+                    "model": model,
+                    "metadata": {"id": sentence['id']},
+                    "input": sentence['combined_sentence']
+                }
+                for sentence in sentences
+            ]
+    with open(filename, "w", encoding="UTF-8") as f:
+        for job in jobs:
+            json_string = json.dumps(job, ensure_ascii=False)
+            f.write(json_string + "\n")
 
 def return_dic(sentences : List[str]) -> List[Dict[str, str|int]]:
     """
@@ -68,6 +83,25 @@ def combine_sentences(sentences: List[Dict[str,str]], buffer_size: int) -> List[
 
     return sentences
 
+def _make_embedding_dict(file_path):
+        returned_dict = {}
+        with open(file_path, 'r') as file:
+            for line in file:
+                data = json.loads(line)
+                indx = data[-1]['id']
+                embedding = data[1]['data'][0]['embedding']
+                returned_dict[indx] = embedding
+        return returned_dict
+
+def save_combined_embeddings_jobs(sentences: List[Dict[str,str]], model_name: str = "text-embedding-3-small") -> None:
+
+    sentence_path = "datasets/cobined_sentences.jsonl"
+    saved_path = "datasets/combined_sentences_embeddings.jsonl"
+    save_jobs(sentences, sentence_path)
+    run_api_request_processor(sentence_path, saved_path, "https://api.openai.com/v1/embeddings")
+    return _make_embedding_dict(saved_path)
+
+
 def generate_embeddings(sentences: List[Dict[str,str]]) -> List[Dict[str,str]]:
     """
     Generates embeddings for combined sentences and adds them to each sentence dictionary.
@@ -89,9 +123,9 @@ def generate_embeddings(sentences: List[Dict[str,str]]) -> List[Dict[str,str]]:
     >>> embeddings = generate_embeddings(sentences)
     >>> print(embeddings)
     """
-    embeddings = oaiembeds.embed_documents([x['combined_sentence'] for x in sentences])
-    for i, sentence in enumerate(sentences):
-        sentence['combined_sentence_embedding'] = embeddings[i]
+    sent_dict = save_combined_embeddings_jobs(sentences)
+    for sentence in sentences:
+        sentence['combined_sentence_embedding'] = sent_dict[sentence['id']]
     
     return sentences
 
@@ -352,21 +386,9 @@ if __name__== "__main__":
     # set environment
     openai.api_key = os.getenv("OPENAI_API_KEY")
     
-
-    contexts = ["Pajton je veoma popularan programski jezik opšte namene. Postao je poznat po svojoj jednostavnosti, lakoći učenja i brzini programiranja.     Mnogi profesionalni programeri koriste Pajton bar kao pomoćni jezik, jer pomoću njega brzo i lako automatizuju razne poslove. ",
-                 "Za izvršavanje programa koje pišemo na Pajtonu, potreban nam je program koji se zove Pajton interpreter. Ovaj program tumači (interpretira), a zatim i izvršava Pajton naredbe. Pajton interpreteri mogu da prihvate cele programe i da ih izvrše, a mogu da rade i u interaktivnom režimu, ",
-                 "Još jedan način da pokrenete Pajton školjku je da otvorite komandni prozor (na Windows sistemima to se radi pokretanjem programa cmd), a zatim u komandnom prozoru otkucate Python (ovde podrazumevamo da je Pajton instaliran tako da je dostupan iz svakog foldera, u protivnom treba se prvo pozicionirati u folder u kome se nalazi Pajton interpreter).",
-                 "Novi Sad je Evropska prestonica kulture 2022. Novi Sad je, posle Beograda, drugi grad u Srbiji po broju stanovnika (bez podataka za područje Kosova i Metohije). Na poslednjem zvaničnom popisu iz 2011. godine, sam grad je imao 231.798[2] stanovnika. Na opštinskom području Novog Sada (uključujući i prigradska nasenja) broj stanovnika je 2011. godine iznosio 341.625.[",
-                 "Najstariji arheološki ostaci (iz vremena kamenog doba) pronađeni su sa obe strane Dunava, na području današnjeg Petrovaradina (koji je u kontinuitetu nastanjen od praistorije do danas) i području današnje Klise. Istraživanjem ostataka naselja iz mlađeg bronzanog doba (3000. godina pne.) na području današnjeg Petrovaradina, arheolozi su pronašli i bedeme pojačane koljem i palisadama iz tog perioda, koji svedoče da je još u vreme vučedolske kulture ovde postojalo utvrđeno naselje.",
-                 "Sredinom 6. veka, područje naseljavaju Sloveni. U 9. veku, tvrđava Petrikon (ili na slovenskim jezicima - Petrik) ulazi u sastav Bugarskog carstva, a u 11. veku njom vlada sremski vojvoda Sermon, čiji su zlatnici u 19. veku pronađeni u jednom petrovaradinskom vinogradu. Pošto je Bugarska poražena od Vizantije a Sermon ubijen, tvrđava ponovo postaje deo Vizantije, da bi, posle borbi Vizantinaca i Mađara, krajem 12. veka, ušla u sastav srednjovekovne Kraljevine Ugarske. Na području Bačke, ugarska vlast se ustaljuje nešto ranije, tokom 10. veka.",
-                 "U vreme Osmanske uprave poznat pod imenom Varadin, Petrovaradin je bio sedište nahije u okviru Sremskog sandžaka. Podgrađe tvrđave imalo je oko 200 kuća, tu se nalazila Sulejman-hanova džamija, a postojale su i dve manje džamije, Hadži-Ibrahimova i Huseinova. Pored dve turske mahale, u sastavu grada nalazila se i hrišćanska četvrt sa 35 kuća, naseljenih isključivo Srbima.[8] Od praistorije pa sve do kraja 17. veka, centar urbanog života na području današnjeg grada nalazio se na sremskoj strani Dunava, na prostoru današnjeg Petrovaradina, koji je svojim značajem uvek zasenjivao naselja na bačkoj strani.",
-                 "Tokom većeg dela 18. i 19. veka Novi Sad je bio središte kulturnog, političkog i društvenog života celokupnog srpskog naroda, koji tada nije imao sopstvenu državu, a prema podacima iz prve polovine 19. veka, ovo je bio i najveći grad nastanjen Srbima (Oko 1820. godine Novi Sad je imao oko 20.000 stanovnika, od kojih su oko dve trećine bili Srbi, a današnji najveći srpski grad, Beograd, nije dostigao približan broj stanovnika pre 1853. godine). Zbog svog kulturnog i političkog uticaja, Novi Sad je postao poznat kao Srpska Atina.",
-                 "Posle sprovedenih izbora po svim vojvođanskim mestima (od 18. do 24. novembra), u Novom Sadu se 25. novembra 1918. godine sastala Velika narodna skupština Srba, Bunjevaca i ostalih Slovena Banata, Bačke i Baranje, koja zvanično proglašava otcepljenje ovih regiona od Ugarske i njihovo prisajedinjenje Srbiji, a na istoj skupštini formira se i pokrajinska vlada (Narodna uprava) Banata, Bačke i Baranje sa sedištem u Novom Sadu. 1. decembra 1918. godine, proglašeno je Kraljevstvo Srba, Hrvata i Slovenaca, a Novi Sad ulazi u tu novu državu kao sastavni deo Kraljevine Srbije.",
-                 " Novom Sadu je, tokom celog rata, delovao pokret otpora i oslobodilački pokret pod vođstvom Komunističke Partije Jugoslavije. U gradu se nalazilo sedište okružnog komiteta Komunističke Partije Jugoslavije, koji je u okviru narodnooslobodilačkog pokreta bio deo Pokrajinskog komiteta KPJ za Vojvodinu. U narodnooslobodilačkoj borbi, u partizanskim odredima i vojvođanskim brigadama, neposredno je učestvovalo 2.365 Novosađana, pripadnika svih nacionalnosti, (Srba, Mađara, Slovaka i ostalih)"
-                 ]
-    sentences = []
-    for context in contexts:
-        sentences.extend(context.split("."))
+    with open('chunking_example/chunking_test_example.json', 'r') as file:
+        contexts = json.load(file) 
+    sentences = contexts['contexts']
     chunks = get_chunks(sentences=sentences, buffer_size=1)
     print(len(chunks))
     pprint(chunks)
