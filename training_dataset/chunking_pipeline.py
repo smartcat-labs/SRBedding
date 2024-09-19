cd201c5f1a36c82b3e1d24216a7fcab0f0caa3e6
SelenaMilutin <selena.milutin@gmail.com>
2024-09-19 13:23:29 +0200

    Import fix

--- training_dataset/chunking_pipeline.py
+++ training_dataset/chunking_pipeline.py
@@ -1,35 +1,41 @@
-from datetime import datetime
+import json
 import os
 import sys
+from datetime import datetime
+from pathlib import Path
+from pprint import pprint
+from statistics import median
 from typing import Dict, List
+
 import numpy as np
 import openai
 import tiktoken
-import json
-
 from sklearn.metrics.pairwise import cosine_similarity
-from pprint import pprint
-from statistics import median
-from pathlib import Path
-sys.path.append('..')
+
+sys.path.append("..")
 from api_request_parallel_processor import run_api_request_processor
 
-def save_jobs(sentences: List[Dict[str, str]], filename: Path, model: str = "text-embedding-3-small") -> None:    
 
+def save_jobs(
+    sentences: List[Dict[str, str]],
+    filename: Path,
+    model: str = "text-embedding-3-small",
+) -> None:
     jobs = [
-                {
-                    "model": model,
-                    "metadata": {"id": sentence['id']},
-                    "input": sentence['combined_sentence']
-                }
-                for sentence in sentences
-            ]
+        {
+            "model": model,
+            "metadata": {"id": sentence["id"]},
+            "input": sentence["combined_sentence"],
+        }
+        for sentence in sentences
+    ]
     with open(filename, "w", encoding="UTF-8") as f:
         for job in jobs:
             json_string = json.dumps(job, ensure_ascii=False)
             f.write(json_string + "\n")
 
-def return_dic(sentences : List[str]) -> List[Dict[str, str|int]]:
+
+def return_dic(sentences: List[str]) -> List[Dict[str, str | int]]:
     """
     Generates a list of dictionaries from a list of sentences.
 
@@ -47,14 +53,17 @@ def return_dic(sentences : List[str]) -> List[Dict[str, str|int]]:
         >>> sentences_dic = return_dic(texts)
         >>> print(sentences_dic)
     """
-    sent_lst_dic = [{'sentence': x, 'id' : i} for i, x in enumerate(sentences)]
+    sent_lst_dic = [{"sentence": x, "id": i} for i, x in enumerate(sentences)]
     return sent_lst_dic
 
-def combine_sentences(sentences: List[Dict[str,str]], buffer_size: int) -> List[Dict[str,str]]:
+
+def combine_sentences(
+    sentences: List[Dict[str, str]], buffer_size: int
+) -> List[Dict[str, str]]:
     """
     Combines each sentence in a list with its surrounding sentences based on a buffer size.
 
-    For each sentence, the function concatenates the sentences within the specified buffer size before and after it, 
+    For each sentence, the function concatenates the sentences within the specified buffer size before and after it,
     creating a new key 'combined_sentence' in each dictionary.
 
     Parameters:
@@ -69,50 +78,54 @@ def combine_sentences(sentences: List[Dict[str,str]], buffer_size: int) -> List[
     >>> print(sentences_dic)
     """
     for i in range(len(sentences)):
-        combined_sentence = ''
+        combined_sentence = ""
 
         for j in range(i - buffer_size, i):
             if j >= 0:
-                combined_sentence += sentences[j]['sentence'] + ' '
-        combined_sentence += sentences[i]['sentence']
+                combined_sentence += sentences[j]["sentence"] + " "
+        combined_sentence += sentences[i]["sentence"]
 
         for j in range(i + 1, i + 1 + buffer_size):
             if j < len(sentences):
-                combined_sentence += ' ' + sentences[j]['sentence']
+                combined_sentence += " " + sentences[j]["sentence"]
 
-        sentences[i]['combined_sentence'] = combined_sentence
+        sentences[i]["combined_sentence"] = combined_sentence
 
     return sentences
 
+
 def _make_embedding_dict(file_path):
-        returned_dict = {}
-        with open(file_path, 'r', encoding='UTF-8') as file:
-            for line in file:
-                try:
-                    data = json.loads(line)
-                    indx = data[-1]['id']
-                    embedding = data[1]['data'][0]['embedding']
-                    returned_dict[indx] = embedding
-                except Exception as e:
-                    print("failed " + str(indx))
-                    returned_dict[indx] = [0.0] * 1536
-        return returned_dict
+    returned_dict = {}
+    with open(file_path, "r", encoding="UTF-8") as file:
+        for line in file:
+            try:
+                data = json.loads(line)
+                indx = data[-1]["id"]
+                embedding = data[1]["data"][0]["embedding"]
+                returned_dict[indx] = embedding
+            except Exception as _:
+                print("failed " + str(indx))
+                returned_dict[indx] = [0.0] * 1536
+    return returned_dict
 
-def save_combined_embeddings_jobs(sentences: List[Dict[str,str]], model_name: str = "text-embedding-3-small") -> None:
 
+def save_combined_embeddings_jobs(sentences: List[Dict[str, str]]) -> None:
     date = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
     sentence_path = Path(f"commands/cobined_sentences_{date}.jsonl")
     saved_path = Path(f"commands/combined_sentences_embeddings_{date}.jsonl")
     saved_path.parent.mkdir(parents=True, exist_ok=True)
     save_jobs(sentences, sentence_path)
-    run_api_request_processor(sentence_path, saved_path, "https://api.openai.com/v1/embeddings")
+    run_api_request_processor(
+        sentence_path, saved_path, "https://api.openai.com/v1/embeddings"
+    )
     return _make_embedding_dict(saved_path)
 
-def generate_embeddings(sentences: List[Dict[str,str]]) -> List[Dict[str,str]]:
+
+def generate_embeddings(sentences: List[Dict[str, str]]) -> List[Dict[str, str]]:
     """
     Generates embeddings for combined sentences and adds them to each sentence dictionary.
 
-    The function uses the `oaiembeds.embed_documents` method to generate embeddings for the 'combined_sentence' 
+    The function uses the `oaiembeds.embed_documents` method to generate embeddings for the 'combined_sentence'
     and appends the resulting embeddings as a new key 'combined_sentence_embedding' in each dictionary.
 
     Parameters:
@@ -131,11 +144,14 @@ def generate_embeddings(sentences: List[Dict[str,str]]) -> List[Dict[str,str]]:
     """
     sent_dict = save_combined_embeddings_jobs(sentences)
     for sentence in sentences:
-        sentence['combined_sentence_embedding'] = sent_dict[sentence['id']]
-    
+        sentence["combined_sentence_embedding"] = sent_dict[sentence["id"]]
+
     return sentences
 
-def calculate_cosine_distances(sentences: List[Dict[str,str]]) -> tuple[List[int], List[str]]:
+
+def calculate_cosine_distances(
+    sentences: List[Dict[str, str]],
+) -> tuple[List[int], List[str]]:
     """
     Calculates cosine distances between the embeddings of consecutive sentences and adds the distance to each sentence dictionary.
 
@@ -146,7 +162,7 @@ def calculate_cosine_distances(sentences: List[Dict[str,str]]) -> tuple[List[int
     sentences (List[Dict[str, str]]): A list of dictionaries, each containing a 'combined_sentence_embedding' key.
 
     Returns:
-    Tuple[List[float], List[Dict[str, str]]]: 
+    Tuple[List[float], List[Dict[str, str]]]:
         - A list of cosine distances between consecutive sentence embeddings.
         - The updated list of dictionaries with an additional 'distance_to_next' key.
 
@@ -159,24 +175,27 @@ def calculate_cosine_distances(sentences: List[Dict[str,str]]) -> tuple[List[int
     >>> print(updated_sentences)
     """
     distances = []
-    
+
     for i in range(len(sentences) - 1):
-        embedding_current = sentences[i]['combined_sentence_embedding']
-        embedding_next = sentences[i + 1]['combined_sentence_embedding']
+        embedding_current = sentences[i]["combined_sentence_embedding"]
+        embedding_next = sentences[i + 1]["combined_sentence_embedding"]
         similarity = cosine_similarity([embedding_current], [embedding_next])[0][0]
         distance = 1 - similarity
         distances.append(distance)
 
-        sentences[i]['distance_to_next'] = distance
+        sentences[i]["distance_to_next"] = distance
 
     return distances, sentences
 
-def get_breakpoint(distances: List[int], sentences: List[str], threshold: int) -> List[str]:
+
+def get_breakpoint(
+    distances: List[int], sentences: List[str], threshold: int
+) -> List[str]:
     """
-    Splits a list of sentences into chunks based on a distance threshold. 
+    Splits a list of sentences into chunks based on a distance threshold.
     If you want more chunks, lower the percentile cutoff.
 
-    The function identifies breakpoints in the sentence list where the cosine distances between sentence embeddings 
+    The function identifies breakpoints in the sentence list where the cosine distances between sentence embeddings
     exceed a specified percentile threshold. It then splits the sentences into chunks at those breakpoints.
 
     Parameters:
@@ -198,23 +217,26 @@ def get_breakpoint(distances: List[int], sentences: List[str], threshold: int) -
     >>> print(chunks)
     """
     breakpoint_distance_threshold = np.percentile(distances, threshold)
-    indices_above_thresh = [i for i, x in enumerate(distances) if x > breakpoint_distance_threshold]
+    indices_above_thresh = [
+        i for i, x in enumerate(distances) if x > breakpoint_distance_threshold
+    ]
     start_index = 0
     chunks = []
 
     for index in indices_above_thresh:
         end_index = index
-        group = sentences[start_index:end_index + 1]
-        combined_text = ' '.join([d['sentence'] for d in group])
+        group = sentences[start_index : end_index + 1]
+        combined_text = " ".join([d["sentence"] for d in group])
         chunks.append(combined_text)
-        
+
         start_index = index + 1
 
     if start_index < len(sentences):
-        combined_text = ' '.join([d['sentence'] for d in sentences[start_index:]])
+        combined_text = " ".join([d["sentence"] for d in sentences[start_index:]])
         chunks.append(combined_text)
 
     return chunks
+
 
 def num_tokens_from_string(string: str, encoding_name: str) -> int:
     """
@@ -239,12 +261,13 @@ def num_tokens_from_string(string: str, encoding_name: str) -> int:
     num_tokens = len(encoding.encode(string))
     return num_tokens
 
+
 def get_threshold(distances: List[int], sentences: List[str]) -> int:
     """
     Determines the optimal threshold value for splitting sentences into chunks based on token length.
 
     The function iterates over a range of threshold values, using each one to generate chunks of sentences.
-    It then calculates the median token length of the chunks and returns the first threshold that results in 
+    It then calculates the median token length of the chunks and returns the first threshold that results in
     a median chunk length greater than 90 tokens. If no such threshold is found, it returns 90.
 
     Parameters:
@@ -265,19 +288,22 @@ def get_threshold(distances: List[int], sentences: List[str]) -> int:
     >>> print(threshold)
     """
     for threshold in range(40, 100, 5):
-        breaks = get_breakpoint(distances=distances, sentences=sentences, threshold=threshold)
-        
+        breaks = get_breakpoint(
+            distances=distances, sentences=sentences, threshold=threshold
+        )
+
         chunk_lengths = []
         for chunk in breaks:
-            tokens = num_tokens_from_string(chunk, 'cl100k_base')
+            tokens = num_tokens_from_string(chunk, "cl100k_base")
             chunk_lengths.append(tokens)
 
         median_length = median(chunk_lengths)
 
         if median_length > 110:
             return threshold
- 
+
     return 90
+
 
 def split_chunk(big_chunk: str, smallest_size: int, largest_size: int) -> List[str]:
     """
@@ -302,30 +328,32 @@ def split_chunk(big_chunk: str, smallest_size: int, largest_size: int) -> List[s
     sentences = big_chunk.split(".")
     current_sentence = ""
     for sentence in sentences:
-        token_size = num_tokens_from_string(current_sentence, 'cl100k_base')
-        if smallest_size < token_size <largest_size:
+        token_size = num_tokens_from_string(current_sentence, "cl100k_base")
+        if smallest_size < token_size < largest_size:
             splits.append(current_sentence)
-            current_sentence = ''
-        elif token_size>=largest_size:
+            current_sentence = ""
+        elif token_size >= largest_size:
             current_sentence = ""
         else:
             current_sentence += "." + sentence
     return splits
 
+
 def remove_start_dot(chunks):
     updated_chunks = []
     for chunk in chunks:
-        if chunk.startswith('.'):
+        if chunk.startswith("."):
             chunk = chunk[1:].strip()
         updated_chunks.append(chunk)
     return updated_chunks
+
 
 def get_filtered_chunks(chunks: List[str]) -> List[str]:
     """
     Filters and refines chunks of text based on token size.
 
-    The function processes a list of text chunks, filtering out those with fewer than 50 tokens and splitting 
-    those with more than 450 tokens into smaller chunks. It returns a list of chunks that each contain between 
+    The function processes a list of text chunks, filtering out those with fewer than 50 tokens and splitting
+    those with more than 450 tokens into smaller chunks. It returns a list of chunks that each contain between
     50 and 450 tokens.
 
     Parameters:
@@ -347,14 +375,14 @@ def get_filtered_chunks(chunks: List[str]) -> List[str]:
     largest_size = 500
     filtered = []
     for chunk in chunks:
-        token_num = num_tokens_from_string(chunk, 'cl100k_base')
+        token_num = num_tokens_from_string(chunk, "cl100k_base")
         if token_num < smallest_size:
             continue
         if token_num < largest_size:
             filtered.append(chunk)
         else:
             filtered.extend(split_chunk(chunk, smallest_size, largest_size))
-    return remove_start_dot(filtered) 
+    return remove_start_dot(filtered)
 
 
 def get_chunks(sentences: List[str], buffer_size: int) -> List[str]:
@@ -398,14 +426,16 @@ def get_chunks(sentences: List[str], buffer_size: int) -> List[str]:
     chunks = get_filtered_chunks(sentences_breaks)
     return chunks
 
-if __name__== "__main__":
 
+if __name__ == "__main__":
     # set environment
     openai.api_key = os.getenv("OPENAI_API_KEY")
-    
-    with open('chunking_example/chunking_test_example.json', 'r', encoding='UTF-8') as file:
-        contexts = json.load(file) 
-    sentences = contexts['contexts']
+
+    with open(
+        "chunking_example/chunking_test_example.json", "r", encoding="UTF-8"
+    ) as file:
+        contexts = json.load(file)
+    sentences = contexts["contexts"]
     chunks = get_chunks(sentences=sentences, buffer_size=1)
     print(len(chunks))
     pprint(chunks)
--- training_dataset/main_training.py
+++ training_dataset/main_training.py
@@ -1,20 +1,21 @@
-import random
 from pathlib import Path
 from typing import List
 
-from chunking_pipeline import get_chunks
-from datasets_loading import get_datasets
 # from query_creation_pipeline import generate_query
 from batch_query_creation import generate_query
+from chunking_pipeline import get_chunks
+from datasets_loading import get_datasets
 
 
-def make_subset_from_all_sentences(all_sentences: List[str], final_lenght: int, chunked_lenght:int, jump: int) -> List[str]:
+def make_subset_from_all_sentences(
+    all_sentences: List[str], final_lenght: int, chunked_lenght: int, jump: int
+) -> List[str]:
     """
     Creates a subset of sentences by selecting chunks from the input list with random steps.
 
-    This function selects chunks of sentences from the input list `all_sentences`, adding 
-    them to the final list until the desired number of sentences is reached or until the 
-    input list is exhausted. The chunks are selected based on the `chunked_lenght`, and 
+    This function selects chunks of sentences from the input list `all_sentences`, adding
+    them to the final list until the desired number of sentences is reached or until the
+    input list is exhausted. The chunks are selected based on the `chunked_lenght`, and
     a random step size determines the starting point of the next chunk.
 
     Args:
@@ -33,27 +34,36 @@ def make_subset_from_all_sentences(all_sentences: List[str], final_lenght: int, 
     """
     start = 0
     final_sentences = []
-    while start+chunked_lenght < len(all_sentences) and len(final_sentences) < final_lenght:
+    while (
+        start + chunked_lenght < len(all_sentences)
+        and len(final_sentences) < final_lenght
+    ):
         sentences = [
-            sentence for sentence in all_sentences[start:start+chunked_lenght]
+            sentence
+            for sentence in all_sentences[start : start + chunked_lenght]
             if len(sentence) < 100_000
         ]
         final_sentences.extend(sentences)
         start += chunked_lenght + jump
     return final_sentences
 
-if __name__== "__main__":
+
+if __name__ == "__main__":
     datasets = get_datasets()
 
     for dataset_name, dataset_args in datasets.items():
-        all_sentences =dataset_args['loading_function']()
+        all_sentences = dataset_args["loading_function"]()
         print(dataset_name)
-        filthered_sentences = make_subset_from_all_sentences(all_sentences=all_sentences,
-                                                        final_lenght = 50,
-                                                        chunked_lenght = 40,
-                                                        # final_lenght = dataset_args['final_lenght'],
-                                                        # chunked_lenght = dataset_args['chunked_lenght'],
-                                                        jump = dataset_args['jump'],
-                                                        )
+        filthered_sentences = make_subset_from_all_sentences(
+            all_sentences=all_sentences,
+            final_lenght=50,
+            chunked_lenght=40,
+            # final_lenght = dataset_args['final_lenght'],
+            # chunked_lenght = dataset_args['chunked_lenght'],
+            jump=dataset_args["jump"],
+        )
         finall_contexts = get_chunks(filthered_sentences, buffer_size=2)
-        generate_query(contexts=finall_contexts, save_filepath=Path(f"datasets/{dataset_name}_train.parquet"))
+        generate_query(
+            contexts=finall_contexts,
+            save_filepath=Path(f"datasets/{dataset_name}_train.parquet"),
+        )
